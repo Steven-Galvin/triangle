4cff80b659e0f3dedcff5813b636bbbb0cd95d54
Jennifer Kinsey and Steven Galvin <jennifer@>
2017-04-24 10:10:31 -0700

    test and pass first two specs. app returns false if not a triangle and true if is a triangle.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,11 @@
 class Triangle
   define_method(:initialize) do |side1, side2, side3|
-    #code goes here
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
+  define_method(:triangle?) do
+    (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
+  end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -7,5 +7,28 @@ describe(Triangle) do
       test_triangle = Triangle.new(1, 1000, 1)
       expect(test_triangle.triangle?()).to(eq(false))
     end
+
+    it("returns true if it is a triangle") do
+      test_triangle = Triangle.new(1, 1, 1)
+      expect(test_triangle.triangle?()).to(eq(true))
+    end
+  end
+
+  describe("#equilateral?") do
+    it("returns true if triangle is an equilateral") do
+      test_triangle = Triangle.new(2, 2, 2)
+      expect(test_triangle.equilateral?()).to(eq(true))
+    end
   end
 end
+
+
+
+# validation:
+# cannot have 0 as a value
+# the sum of the lengths of any two sides of a triangle is less than
+#or equal to the length of the third side.
+#
+# Equilateral: All sides are equal.
+# Isosceles: Exactly 2 sides are equal.
+# Scalene: No sides are equal.
