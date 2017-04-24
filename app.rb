bef35fe70b6fd98327369679e51d684775b8efd5
Jennifer Kinsey and Steven Galvin <jennifer@>
2017-04-24 11:30:34 -0700

    corrected app.rb fetch params to convert string input to integer

--- app.rb
+++ app.rb
@@ -8,9 +8,10 @@ get('/') do
 end
 
 get('/triangle') do
-  side1 = params.fetch('side1')
-  side2 = params.fetch('side2')
-  side3 = params.fetch('side3')
+  side1 = params.fetch('side1').to_i
+  side2 = params.fetch('side2').to_i
+  side3 = params.fetch('side3').to_i
   @triangle = Triangle.new(side1, side2, side3)
+
   erb(:triangle)
 end
